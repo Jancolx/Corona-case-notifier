d433cf7050f897b9875a13746a018e55e8a20a84
Jancol <bhabanisankarray5@gmail.com>
2021-05-02 10:51:20 +0530

    done

--- coronanotifier.py
+++ coronanotifier.py
@@ -1,43 +1,32 @@
-import requests
 from plyer import notification
+import requests
 from bs4 import BeautifulSoup
 from bs4 import Comment
 
-def notifye(title, message): 
+def notifyMe(title,message):
     notification.notify(
-        
         title=title,
-        
         message=message,
-
-        ticker="coronanotifier", app_icon="C://Users//Jancol//Desktop//New folder//coronanotifier. timeout-18
-
-
+        timeout=15
+    )
 
 def getData(url):
-
-    r=requests.get(url) 
-    
+    r=requests.get(url)
     return r.text
 
+mydata=getData("https://www.mygov.in/covid-19/")
+soup=BeautifulSoup(mydata,'html.parser')
+x=soup.find_all('div',{"class":"views-row"})
+for i in x :
 
+    state= i.find('span',{"class":"st_name"})
+    if state.get_text() == "Odisha":
+        active= i.find('div',{"class":"tick-active"})
+        msg1 = "Active Cases : "+ active.get_text()[7: ]
+        vaccine=i.find('div',{"class":"tick-total-vaccine"})
+        msg2="Vaccination: "+ vaccine.get_text()[12: ]
 
-mydata-getData("https://www.mygov.in/covid-19/") 
-
-soup=BeautifulSoup(mydata, 'html.parser")
-
-x=soup.find_all('div'.{"class":"views-row"}) 
-
-for i in x:
-
-    s= i.find('span',{"class":"st_namo"})
-
-    if s.get_text() == "Odisha": 
-        y=i.find('div',{"class":"tick-active"}) 
-        msgl="Active Cases: "+y.get_text()[7: ]
-        z=i.find("div",{"class": "tick-total-vaccine"})
-        msg2-"Vaccination: "+z.get_text()[12: ]
-
-title="Odisha"
-msg=msg1+"\n"+msg2 
-notifyMe(title,msg)   
+    
+title ="Odisha"
+msg=msg1+"\n"+msg2
+notifyMe(title,msg)
